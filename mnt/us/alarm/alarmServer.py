85dfc512d0885a9c7f800548bd4f150c91b17c98
Philipp Mundhenk <philipp@mundhenk.org>
2017-06-07 17:40:44 +0200

    reset to start with 0 volume

--- mnt/us/alarm/alarmServer.py
+++ mnt/us/alarm/alarmServer.py
@@ -104,7 +104,7 @@ class RestHTTPRequestHandler(BaseHTTPRequestHandler):
 		global secondsToAutoOff
 		time.sleep(i-10)
 		self.WifiOn()
-		command = "(/mnt/us/mplayer/mplayer -loop 0 -cache 1024 -volume 100 -playlist /mnt/us/alarm/playlist.m3u -input file=/tmp/test.fifo -ao alsa -slave -quiet </dev/null >/mnt/us/alarm/log_mplayer.log 2>&1)&"
+		command = "(/mnt/us/mplayer/mplayer -loop 0 -cache 1024 -volume 0 -playlist /mnt/us/alarm/playlist.m3u -input file=/tmp/test.fifo -ao alsa -slave -quiet </dev/null >/mnt/us/alarm/log_mplayer.log 2>&1)&"
 		os.system(command)
 		#command = "(sleep 1 && amixer sset 'Speaker' 0)&"
 		command = "(sleep 1 && echo \"set_property volume 0\" > /tmp/test.fifo)&"
